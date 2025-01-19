2791f0e379587d44c339ac2d3331d052e831eda0
UBC Student <axu03@students.cs.ubc.ca>
2025-01-18 16:49:48 -0800

    insert items into database

--- backend/database.sql
+++ backend/database.sql
@@ -20,7 +20,30 @@ CREATE TABLE items (
 INSERT INTO users (gmail) VALUES ("axuotaku@gmail.com");
 
 INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
-VALUES (1, 'Zucchini Green', '2023-11-08', 5, NULL);
+VALUES (1, 'Zucchini Green', '2023-11-08', NULL, 0.778);
 
 INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
-VALUES (1, 'Banana Cavendish', '2023-11-03', 7, NULL);
+VALUES (1, 'Banana Cavendish', '2023-11-03', NULL, 0.442);
+
+INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
+VALUES (1, 'Potatoes Brushed', '2023-11-11', NULL, 1.328);
+
+INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
+VALUES (1, 'Broccoli', '2023-11-06', NULL, 0.808);
+
+INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
+VALUES (1, 'Brussel Sprouts', '2023-11-07', NULL, 0.322);
+
+INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
+VALUES (1, 'Grapes Green', '2023-11-05', NULL, 1.174);
+
+INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
+VALUES (1, 'Peas Snow', '2023-11-04', NULL, 0.218);
+
+INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
+VALUES (1, 'Tomatoes Grape', '2023-11-07', 2, NULL);
+
+INSERT INTO items (user_id, name, expiry_date, quantity, weight) 
+VALUES (1, 'Lettuce Iceberg', '2023-11-05', 2, NULL);
+
+
