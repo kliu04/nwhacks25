9603a376392339eead63f39083c49b5b61f58a49
Kevin <kevinliu04@gmail.com>
2025-01-19 00:45:15 -0800

    fix deploy bug

--- frontend/src/Pages/Recipes.tsx
+++ frontend/src/Pages/Recipes.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import "./Recipes.css"; // Import the updated CSS
 
 interface Recipe {
