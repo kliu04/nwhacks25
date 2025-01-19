d4911f04835bda0fd187c2bfde9f984307750176
Kevin <kevinliu04@gmail.com>
2025-01-19 01:08:41 -0800

    delete getstarted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,26 +6,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ViewInventory from './Pages/Inventory.tsx';
 
 import Home from './Pages/Home.tsx';
-import GetStarted from './Pages/GetStarted.tsx';
 import UploadReceipt from './Pages/UploadReceipt.tsx';
 import SignInPage from './Pages/SignInPage.tsx';
 import ConfirmationPage from './Pages/ConfirmationPage.tsx';
 import Recipes from './Pages/Recipes.tsx';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/inventory" element={<ViewInventory />} />
-        <Route path="/getstarted" element={<GetStarted />} />
-        <Route path="/upload-receipt" element={<UploadReceipt />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/confirmation" element={<ConfirmationPage />} />
-        <Route path="/recipes" element={<Recipes />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/inventory" element={<ViewInventory />} />
+                <Route path="/upload-receipt" element={<UploadReceipt />} />
+                <Route path="/signin" element={<SignInPage />} />
+                <Route path="/confirmation" element={<ConfirmationPage />} />
+                <Route path="/recipes" element={<Recipes />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
